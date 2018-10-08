6926a7f43e43e56148c746e8bc22874d08ae0d2c
SoBeRBot94 <sudarsan.bhargavan94@gmail.com>
2018-10-08 12:34:03 +0200

    Updated Flask App to Return Container Hostname

--- cowsay.py
+++ cowsay.py
@@ -3,14 +3,16 @@
 from flask import Flask, jsonify
 import subprocess
 import sys
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/cowsay/api/v1.0/saysomething', methods=['GET'])
 def cow_say():
+    containerHostname = os.uname().nodename
     data=subprocess.check_output(["cowsay","Hello student"])
-    return data
+    return data, containerHostname
 
 if __name__ == '__main__':
     
